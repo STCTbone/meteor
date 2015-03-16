ae8616808cbf8c2d81a8ed4cd7d5a16401b764d3
Matthew Rieger <matthew.rieger@gmail.com>
2015-03-15 21:38:50 -0500

    Finished Chapter 9

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,15 @@
+Template.postSubmit.created = function() {
+    Session.set('postSubmitErrors', {});
+};
+
+Template.postSubmit.helpers({
+    errorMessage: function(field) {
+        return Session.get('postSubmitErrors')[field];
+    },
+    errorClass: function(field) {
+        return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+    }
+});
 Template.postSubmit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -7,10 +19,14 @@ Template.postSubmit.events({
             title: $(e.target).find('[name=title]').val()
         };
 
-        Meteor.call('postInsert', post, function(error, result) {
-            if (error) return alert(error.reason);
+        var errors = validatePost(post);
+        if (errors.title || errors.url)
+            return Session.set('postSubmitErrors', errors);
 
-            if (result.postExists) alert('This link has already been posted');
+        Meteor.call('postInsert', post, function(error, result) {
+            if (error) return throwError(error.reason);
+
+            if (result.postExists) throwError('This link has already been posted');
             Router.go('postPage', {_id: result._id});
         });
     }
