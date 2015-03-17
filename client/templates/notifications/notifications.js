b7687f79c1e996bc7f814f2a7c9217c17926dcdd
Matt Rieger <mrieger@mrskin.com>
2015-03-17 09:53:02 -0500

    Finished Chapter 11

---
+++ client/templates/notifications/notifications.js
@@ -0,0 +1,14 @@
+Template.notifications.helpers({
+    notifications: function() {
+        return Notifications.find({userId: Meteor.userId(), read: false});
+    },
+    notificationCount: function() {
+        return Notifications.find({userId: Meteor.userId(), read:false}).count();
+    }
+});
+
+Template.notificationItem.helpers({
+    notificationPostPath: function() {
+        return Router.routes.postPage.path({_id: this.postId});
+    }
+});
